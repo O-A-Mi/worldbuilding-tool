879c4eb531c1d6dfb93ac38d216305560fe0b231
Luiz Miguel <luiz.alves.frr@gmail.com>
2025-10-08 17:58:20 -0300

    Criação da home

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { Routes, Route, Link, BrowserRouter, useLocation } from 'react-router';
 import Login from './pages/Login';
+import HomePage from './pages/WorldProject';
 
 function App() {
   return (
     <Routes>
       <Route index path="/" element={<Login />} />
+      <Route index path="/world-tool" element={<HomePage />} />
     </Routes>
   );
 }
